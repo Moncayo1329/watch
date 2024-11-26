2384387b9261f2da5be1854f2537a57b48e09e61
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-11-26 11:02:56 +0000

    adding styles

--- client/src/Movie.js
+++ client/src/Movie.js
@@ -6,6 +6,7 @@ function Movies({ task }) {
       <img src={task.image} alt={task.title} className="movie-poster" />
       <h3 className="movie-title">{task.title}</h3>
       <p className="movie-description">{task.description}</p> {/* Accede a la descripción de la película */}
+     
     </div>
   );
 }
